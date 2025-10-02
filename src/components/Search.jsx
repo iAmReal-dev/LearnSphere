eb32b8057f5441fadc19b8c41ea6d72901ce8cd3
nitindahiya-dev <nitindahiya00000@gmail.com>
2025-10-03 00:29:37 +0530

    -- update ui

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,11 +6,11 @@ const Search = () => {
             <input
                 type="text"
                 placeholder="What do you want to learn today?"
-                className="placeholder:font-poppins placeholder:text-[#777584] placeholder:text-[16px] bg-transparent text-white placeholder-gray-300 outline-none px-4 py-2 w-full"
+                className="placeholder:font-poppins placeholder:text-[#777584] text-[#777584] placeholder:text-[16px] bg-transparent placeholder-gray-300 outline-none px-4 py-2 w-full"
             />
 
-            <button className="cursor-pointer font-poppins bg-gradient-to-r from-[#57029C] to-[#F700AC] text-white rounded-full px-4 py-2 flex items-center space-x-2 hover:opacity-90 transition duration-300">
-                <span className="w-40">Search</span>
+            <button className="cursor-pointer font-poppins bg-gradient-to-r from-[#57029C] to-[#F700AC] text-white rounded-full px-2 py-1 md:px-4 md:py-2 flex items-center space-x-2 hover:opacity-90 transition duration-300">
+                <span className="w-30 md:w-40">Search</span>
             </button>
 
         </div>
